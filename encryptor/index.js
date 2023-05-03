df59aeb9d4b3646e4acd02c6613ca3d153a0e58f
JJoBro <128638809+JJoBro@users.noreply.github.com>
2023-05-03 13:00:11 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -1,1 +1,10 @@
 console.log('the encryptor js file loaded');
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
+
+//console.log(letters[0]); // a
+
+for (let i = 0; i < letters.length; i++) {
+    console.log(letters[i]);
+}
