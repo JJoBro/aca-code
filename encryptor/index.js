455e1cbc54f3c1a0def98932041a06747e0f9aaf
JJoBro <128638809+JJoBro@users.noreply.github.com>
2023-05-17 12:40:17 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -1,43 +1,58 @@
-console.log('the encryptor js file loaded');
+console.log('The encryptor js file loaded'); 
 
-const letters =     'abcdefghijklmnopqrstuvwxyz!?';
-const encrypt_key = 'bcdefghijklmnopqrstuvwxyza?!';
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
 
 function encrypt(text) {
-let result = '';  
+  let result = '';
   
-  for (let i = 0; i < text.lenght; i++) {
-    let letter = text [i];
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i];
     let letterIndex = letters.indexOf(letter);
     
-    if (lettersIndex < 0) {
+    if (letterIndex < 0) {
       result += letter;
-    }
+    }// if
     
-    else{
-      result += encrypt_key[letterIndex];
-   }
- }
-return result;
-
-}
-
-
+    else {
+      result += encrypt_key[letterIndex]; 
+    }// else
+    
+  }// for
+  
+  return result;
+  
+}// encrypt
 
 
 function decrypt(text) {
-let result = '';  
+  let result = '';
   
-  for (let i = 0; i < text.lenght; i++) {
-    let letter = text [i];
-    let letterIndex = letters.indexOf(letter);
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i];
+    let letterIndex = encrypt_key.indexOf(letter);
     
-    if (lettersIndex < 0) {
+    if (letterIndex < 0) {
       result += letter;
-    }
+    }// if
     
-    else{
-      result += letters[letterIndex];
-   }
- }
-return result;
+    else {
+      result += letters[letterIndex]; 
+    }// else
+    
+  }// for
+  
+  return result;
+  
+}// decrypt
+
+const encryptForm = document.getElementById("encrypt-form")
+encryptForm.addEventListener("submit" , function(e){
+e.preventDefault();
+  let messageElement = document.getElementById("encrypt");
+  let message = messageElement.value;
+  let encrypted = encrypt(message);
+  let encodedMessageElmentdocument.getElementById("encrypt-message");
+  encodedMessageElement.innerText = encrypted;
+});
+                                ; 
